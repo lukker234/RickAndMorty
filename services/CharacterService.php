2c72ce06c5ab350ce034086dd10e1d23e2b74be8
Luc Daalmeijer <l.daalmeijer@ruitenburg.nu>
2024-04-23 14:10:39 +0200

    BAX - Create a dropdown for episodes, and show information about that episode when selecting it

--- services/CharacterService.php
+++ services/CharacterService.php
@@ -3,25 +3,44 @@
 namespace services;
 
 use GuzzleHttp\Client;
-use GuzzleHttp\Exception\GuzzleException;
 
 class CharacterService
 {
-    public function guzzleClient(string $method, string $url): string
+    public function guzzleClient(string $method, string $url): array
     {
         $client = new Client();
         $response = $client->request($method, $url);
 
-        return $response->getBody()->getContents();
+        // Get the body content as a string
+        $bodyContent = $response->getBody()->getContents();
+
+        // Decode the JSON string into an associative array
+        $decodedResponse = json_decode($bodyContent, true);
+
+        if ($decodedResponse === null) {
+            throw new \Exception('Error decoding JSON response from API');
+        }
+
+        return $decodedResponse;
     }
 
-    public function getAllCharacters(): string
+    public function getAllCharacters(): array
     {
         return $this->guzzleClient('GET', 'https://rickandmortyapi.com/api/character');
     }
 
-    public function getAllLocations(): string
+    public function getAllLocations(): array
     {
         return $this->guzzleClient('GET', 'https://rickandmortyapi.com/api/location');
+    }
+
+    public function getAllEpisodes(): array
+    {
+        return $this->guzzleClient('GET', 'https://rickandmortyapi.com/api/episode');
+    }
+
+    public function getEpisodeDetails(): array
+    {
+        return $this->guzzleClient('GET', 'https://rickandmortyapi.com/api/episode');
     }
 }
--- src/Controller/CharacterController.php
+++ src/Controller/CharacterController.php
@@ -5,14 +5,19 @@ namespace App\Controller;
 use services\CharacterService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Annotation\Route;
 
 class CharacterController extends AbstractController
 {
     #[Route('/')]
     public function index(): Response
     {
-        return $this->render('base.html.twig');
+        $characterService = new CharacterService();
+        $episodes = $characterService->getAllEpisodes()['results'];
+
+        return $this->render('base.html.twig', [
+            'episodes' => $episodes,
+        ]);
     }
 
     #[Route('/characters/')]
