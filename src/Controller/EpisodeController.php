1c11e79b6ea0b92c245dd34d873e8154f77f9f76
Luc Daalmeijer <l.daalmeijer@ruitenburg.nu>
2024-04-25 23:05:52 +0200

    BAX - Add refactoring of RickAndMortyApiService to EpisodeController

--- src/Controller/EpisodeController.php
+++ src/Controller/EpisodeController.php
@@ -2,7 +2,7 @@
 
 namespace App\Controller;
 
-use services\CharacterService;
+use services\RickAndMortyApiService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -12,7 +12,7 @@ class EpisodeController extends AbstractController
     #[Route('/')]
     public function episodes(): Response
     {
-        $characterService = new CharacterService();
+        $characterService = new RickAndMortyApiService();
         $episodes = $characterService->getAllEpisodes()['results'];
 
         return $this->render('episodes/episodes.html.twig', [
