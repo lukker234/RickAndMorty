74e4fa2fb4e265920abd35a80e3838cea05e36fe
Luc Daalmeijer <l.daalmeijer@ruitenburg.nu>
2024-04-25 23:03:56 +0200

    BAX - (WIP) Display character details

--- src/Controller/CharacterController.php
+++ src/Controller/CharacterController.php
@@ -2,31 +2,32 @@
 
 namespace App\Controller;
 
-use services\CharacterService;
+use services\RickAndMortyApiService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class CharacterController extends AbstractController
 {
-    #[Route('/')]
-    public function index(): Response
-    {
-        $characterService = new CharacterService();
-        $episodes = $characterService->getAllEpisodes()['results'];
-
-        return $this->render('base.html.twig', [
-            'episodes' => $episodes,
-        ]);
-    }
-
     #[Route('/characters/')]
     public function allCharacters(): Response
     {
-        $characterService = new CharacterService();
+        $characterService = new RickAndMortyApiService();
+        $characters = $characterService->getAllCharacters()['results'];
 
-        return $this->render('characters.html.twig', [
-            'list' => $characterService->getAllCharacters(),
+        return $this->render('character/character.html.twig', [
+            'characters' => $characters,
+        ]);
+    }
+
+    #[Route('/character/{characterId}')]
+    public function getCharacterInfo(int $characterId): Response
+    {
+        $characterService = new RickAndMortyApiService();
+        $character = $characterService->getSingleCharacter($characterId);
+
+        return $this->render('character/character.html.twig', [
+            'character' => $character,
         ]);
     }
 }
