74f0185ba164cc3a5e64b0a4743992bdb6493a42
Luc Daalmeijer <l.daalmeijer@ruitenburg.nu>
2024-04-28 01:15:06 +0200

    BAX - Add home and previous buttons and change index to named route

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -8,8 +8,8 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class MainController extends AbstractController
 {
-    #[Route('/')]
-    public function episodes(): Response
+    #[Route('/', name: 'index')]
+    public function index(): Response
     {
         return $this->render('index.html.twig');
     }
