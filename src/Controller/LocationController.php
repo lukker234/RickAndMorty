6afa17d6a63fa992582ef6a3a67c97f3ec0e9797
Luc Daalmeijer <l.daalmeijer@ruitenburg.nu>
2024-04-27 21:38:35 +0200

    BAX - List all locations and characters

--- src/Controller/LocationController.php
+++ src/Controller/LocationController.php
@@ -2,9 +2,23 @@
 
 namespace App\Controller;
 
+use services\RickAndMortyApiService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class LocationController extends AbstractController
 {
+    #[Route('/locations')]
+    public function locations(): Response
+    {
+        $characterService = new RickAndMortyApiService();
+        $locations = $characterService->getAllLocations();
 
+//        dd($locations);
+
+        return $this->render('locations/locations.html.twig', [
+            'locations' => $locations,
+        ]);
+    }
 }
